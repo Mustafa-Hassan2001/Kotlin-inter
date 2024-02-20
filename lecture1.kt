f5fdf4f181ee856692c1013daf8c49933e7b077f
Syed Mustafa Hassan <98483655+Mustafa-Hassan2001@users.noreply.github.com>
2024-02-20 16:47:13 +0500

    Create lecture1.kt

---
+++ lecture1.kt
@@ -0,0 +1,34 @@
+fun main(){
+var i = 0
+while (i < 5) {
+  println(i)
+  i++
+} 
+var i = 0
+do {
+  println(i)
+  i++
+  }
+while (i < 5) 
+var i = 0
+while (i < 10) {
+  if (i == 4) {
+    i++
+    continue
+  }
+  println(i)
+  i++
+}
+
+
+var i = 0
+while (i < 10) {
+  println(i)
+  i++
+  if (i == 4) {
+    break
+  }
+}
+
+  
+}
